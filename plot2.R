dc1589a07de3d4d12bf8f9c13621bd1868107f86
nanyuchentw <nelly.chen@gmail.com>
2023-10-01 04:23:47 +0800

    plot2.R

---
+++ plot2.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+df <- read.table(file="household_power_consumption.txt", header = TRUE, sep=";")
+df <- df %>% mutate (Date_Time= paste(Date, Time))
+df$Date <- dmy(df$Date)
+df1 <- df %>% filter (Date ==  ymd("2007-02-01") | Date ==  ymd("2007-02-02") )
+df1$Date_Time <- dmy_hms(df1$Date_Time)
+
+df1$Global_active_power <- as.numeric(df1$Global_active_power)
+
+local <- Sys.getlocale("LC_TIME")
+suppressWarnings(Sys.setlocale("LC_ALL","English"))
+
+png("plot2.png", width=480, height=480)
+plot(df1$Date_Time, df1$Global_active_power, type="l", xlab="",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
+
+Sys.setlocale(locale = local)
