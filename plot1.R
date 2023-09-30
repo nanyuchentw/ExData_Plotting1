2e3c464f2005487c964a09eb4501a665f4baaa66
nanyuchentw <nelly.chen@gmail.com>
2023-10-01 04:22:52 +0800

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+df <- read.table(file="household_power_consumption.txt", header = TRUE, sep=";")
+df$Date <- dmy(df$Date)
+df1 <- df %>% filter (Date ==  ymd("2007-02-01") | Date ==  ymd("2007-02-02") )
+
+df1$Global_active_power <- as.numeric(df1$Global_active_power)
+
+png("plot1.png", width=480, height=480)
+hist(df1$Global_active_power, col = "red", 
+     xlab = "Global Active Power (kilowatts)", main ="Global Active Power")
+dev.off()
